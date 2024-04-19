8bb96e7675d190a61430c726130b1ccd3c657578
axlgoze <rbaxel.f@gmail.com>
2024-04-19 12:03:01 -0600

    4)ADD:rotate1 script

---
+++ rotatePdf.py
@@ -0,0 +1,20 @@
+import fitz  # PyMuPDF
+
+def rotar_pdf(input_file, output_file):
+    # Abre el archivo PDF
+    pdf_document = fitz.open(input_file)
+
+    # Itera sobre cada página y rota 90 grados en sentido antihorario
+    for page_num in range(len(pdf_document)):
+        page = pdf_document[page_num]
+        page.set_rotation(90)
+    
+    # Guarda el PDF rotado en un nuevo archivo
+    pdf_document.save(output_file)
+    pdf_document.close()
+
+# Uso del programa
+input_file = "all.pdf"
+output_file = "all_rotado.pdf"
+rotar_pdf(input_file)
+
